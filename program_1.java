2037b030f0a075c866642939dfc7e8aa5cf5f244
Andrew Garbukas <47329073+agarbukas@users.noreply.github.com>
2019-02-04 11:43:25 -0500

    Update program_1.java

--- program_1.java
+++ program_1.java
@@ -4,7 +4,7 @@ public class program_1 {
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		Scanner input = new Scanner(System.in);//establish scanner
 		
